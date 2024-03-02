e9b0165c77b2917cfe4433f4cd56ec85c63b329a
S4hlo <rfl.magno02@gmail.com>
2024-03-02 08:13:23 -0800

    feature: job option in setup.sh

--- setup.sh
+++ setup.sh
@@ -25,6 +25,15 @@ echo ' REQUIREMENTS:
 '
 
 if ask_yes_no "Do you want to set up GNOME configurations"; then
+    repo_line="deb http://deb.debian.org/debian oldstable main non-free contrib"
+    if grep -Fxq "$repo_line" /etc/apt/sources.list; then
+        echo "Repository line already exists in sources.list. No changes made."
+    else
+        echo "$repo_line" | sudo tee -a /etc/apt/sources.list
+        sudo apt update
+        echo "Repository line added and package lists updated."
+    fi
+
     gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
     # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>j']"
     # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>k']"
@@ -97,22 +106,29 @@ else
 fi
 
 if ask_yes_no "Do you want to install job apps?"; then
-    repo_line="deb http://deb.debian.org/debian oldstable main non-free contrib"
-    if grep -Fxq "$repo_line" /etc/apt/sources.list; then
-        echo "Repository line already exists in sources.list. No changes made."
-    else
-        echo "$repo_line" | sudo tee -a /etc/apt/sources.list
-        sudo apt update
-        echo "Repository line added and package lists updated."
-    fi
-
-    sudo tee /etc/apt/sources.list.d/pritunl.list <<EOF
+    if ! pritunl-client version | grep -q "Pritunl Client"; then
+        sudo tee /etc/apt/sources.list.d/pritunl.list <<EOF
 deb http://repo.pritunl.com/stable/apt bookworm main
 EOF
-    sudo apt install dirmngr
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-    sudo apt update
-    sudo apt install pritunl-client-electron
+        sudo apt install dirmngr
+        sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+        sudo apt update
+        sudo apt install pritunl-client-electron
+    else
+        echo "Pritunl is already installed. Skipping installation."
+    fi
+    echo " ✅- PRITUNL-CLIENT installation finished - (1/3)"
+
+    sudo apt install dbeaver
+    echo " ✅- DBEAVER installation finished - (2/3)"
+
+    curl -1sLf \
+      'https://packages.konghq.com/public/insomnia/setup.deb.sh' \
+      | sudo -E distro=ubuntu codename=focal bash
+    sudo apt-get update
+    sudo apt-get install insomnia
+    echo " ✅- INSOMNIA installation finished - (3/3)"
+
 
 else
     echo " 🟡  Skipping job apps installation"
