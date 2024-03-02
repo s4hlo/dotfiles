377dfcc341e431481577a6638470d4df02a2b170
S4hlo <rfl.magno02@gmail.com>
2024-03-02 00:08:02 -0300

    wip: setting up gnome tiling manager forge

--- setup.sh
+++ setup.sh
@@ -26,10 +26,10 @@ echo ' REQUIREMENTS:
 # GNOME SETTINGS
 if ask_yes_no "Do you want to set up GNOME configurations"; then
     gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>j']"
-    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>k']"
-    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Super><Shift>J']"
-    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Super><Shift>K']"
+    # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>j']"
+    # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>k']"
+    # gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Super><Shift>J']"
+    # gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Super><Shift>K']"
     gsettings set org.gnome.desktop.wm.preferences num-workspaces 3
     gsettings set org.gnome.desktop.interface enable-animations false
     gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
