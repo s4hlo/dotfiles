7ff0d1df5f1f934ae7edfa5ffb33e71f9850875a
S4hlo <rfl.magno02@gmail.com>
2024-03-02 09:24:22 -0800

    feature: create a module for job apps setup

--- setup_modules/setup.sh
+++ setup_modules/setup.sh
@@ -47,30 +47,9 @@ else
 fi
 
 if ask_yes_no "Do you want to install job apps?"; then
-    if ! pritunl-client version | grep -q "Pritunl Client"; then
-        sudo tee /etc/apt/sources.list.d/pritunl.list <<EOF
-deb http://repo.pritunl.com/stable/apt bookworm main
-EOF
-        sudo apt install dirmngr
-        sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-        sudo apt update
-        sudo apt install pritunl-client-electron
-    else
-        echo "Pritunl is already installed. Skipping installation."
-    fi
-    echo " ✅- PRITUNL-CLIENT installation finished - (1/3)"
-
-    sudo apt install dbeaver
-    echo " ✅- DBEAVER installation finished - (2/3)"
-
-    curl -1sLf \
-        'https://packages.konghq.com/public/insomnia/setup.deb.sh' |
-        sudo -E distro=ubuntu codename=focal bash
-    sudo apt-get update
-    sudo apt-get install insomnia
-    echo " ✅- INSOMNIA installation finished - (3/3)"
-
-else
+  . ~/dotfiles/setup_modules/job.sh
+  job_setup
+  else
     echo " 🟡  Skipping job apps installation"
 fi
 
@@ -78,7 +57,7 @@ if ask_yes_no "Do you want to install college apps?"; then
     . ~/dotfiles/setup_modules/college.sh
     college_setup
 else
-    echo " 🟡  Skipping job apps installation"
+    echo " 🟡  Skipping college apps installation"
 fi
 
 if ask_yes_no "Do you want to link the dotfiles?"; then
