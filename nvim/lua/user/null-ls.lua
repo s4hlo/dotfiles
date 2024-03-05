d6b9753965805031111fa61ceea4f753cd70a501
S4hlo <rfl.magno02@gmail.com>
2024-03-04 19:19:52 -0800

    fix(nvim): make lsp work properly with prettier

--- nvim/lua/user/null-ls.lua
+++ nvim/lua/user/null-ls.lua
@@ -2,6 +2,10 @@ local M = {
   "jose-elias-alvarez/null-ls.nvim",
 }
 
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
+
 function M.config()
   local null_ls = require "null-ls"
 
@@ -18,6 +22,32 @@ function M.config()
       -- null_ls.builtins.diagnostics.eslint,
       null_ls.builtins.completion.spell,
     },
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+        -- ! TODO this is a paletive - fix this
+          vim.keymap.set("n", "<Leader>lq", function()
+            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+          end, { buffer = bufnr, desc = "[lsp] format" })
+
+          -- format on save
+          vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+          vim.api.nvim_create_autocmd(event, {
+            buffer = bufnr,
+            group = group,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr, async = async })
+            end,
+            desc = "[lsp] format on save",
+          })
+        end
+
+        if client.supports_method("textDocument/rangeFormatting") then
+          -- ! TODO this is a paletive - fix this
+          vim.keymap.set("x", "<Leader>lq", function()
+            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+          end, { buffer = bufnr, desc = "[lsp] format" })
+        end
+      end,
   }
 end
 
