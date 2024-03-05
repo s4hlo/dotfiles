991be5ef8cc623cae49cbee7d7d3fa86fa94d1d4
S4hlo <rfl.magno02@gmail.com>
2024-03-04 17:55:46 -0800

    refactor(setup): update i3 app setup

--- setup_modules/i3.sh
+++ setup_modules/i3.sh
@@ -1,22 +1,19 @@
 i3_setup() {
     sudo apt install i3
-    echo " ✅- I3 installation finished (1/7)"
+    echo " ✅- I3 installation finished (1/6)"
 
     sudo apt install polybar
-    echo " ✅- POLYBAR installation finished (2/7)"
-
-    sudo apt install brightnessctl
-    echo " ✅- BRIGHTNESSCTL installation finished (3/7)"
-
-    sudo apt install pavucontrol
-    echo " ✅- PAVUCONTROL installation finished (4/7)"
-
-    sudo apt install playerctl
-    echo " ✅- PLAYERCTL installation finished (5/7)"
+    echo " ✅- POLYBAR installation finished (2/6)"
 
     sudo apt install rofi
-    echo " ✅- ROFI installation finished (6/7)"
+    echo " ✅- ROFI installation finished (3/6)"
+
+    sudo apt install brightnessctl
+    echo " ✅- BRIGHTNESSCTL installation finished (4/6)"
+
+    sudo apt install playerctl
+    echo " ✅- PLAYERCTL installation finished (5/6)"
 
     sudo apt install xwallpaper
-    echo " ✅- XWALLPAPER installation finished (7/7)"
+    echo " ✅- XWALLPAPER installation finished (6/6)"
 }
