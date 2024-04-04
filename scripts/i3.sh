dc3a0442625bac049718d1ab1035555a4447245e
S4hlo <rfl.magno02@gmail.com>
2024-04-04 08:19:40 -0700

    feature(picom): add picom and make emoji work OMG

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -1,19 +1,23 @@
 i3_setup() {
     sudo apt install i3
-    echo " ✅- I3 installation finished (1/6)"
+    echo " ✅- I3 installation finished (1/7)"
 
     sudo apt install polybar
-    echo " ✅- POLYBAR installation finished (2/6)"
+    echo " ✅- POLYBAR installation finished (2/7)"
 
     sudo apt install rofi
-    echo " ✅- ROFI installation finished (3/6)"
+    echo " ✅- ROFI installation finished (3/7)"
 
     sudo apt install brightnessctl
-    echo " ✅- BRIGHTNESSCTL installation finished (4/6)"
+    echo " ✅- BRIGHTNESSCTL installation finished (4/7)"
 
     sudo apt install playerctl
-    echo " ✅- PLAYERCTL installation finished (5/6)"
+    echo " ✅- PLAYERCTL installation finished (5/7)"
 
     sudo apt install xwallpaper
-    echo " ✅- XWALLPAPER installation finished (6/6)"
+    echo " ✅- XWALLPAPER installation finished (6/7)"
+
+    sudo apt install picom
+    echo " ✅- PICOM installation finished (7/7)"
+
 }
