93faee4bbf09a2f509d100267cd03abe1ac808ea
S4hlo <rfl.magno02@gmail.com>
2024-05-01 00:48:12 +0000

    feat(nvim): add notifications

--- nvim/lua/user/noice.lua
+++ nvim/lua/user/noice.lua
@@ -10,7 +10,8 @@ local M = {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		-- "rcarriga/nvim-notify",
+		"echasnovski/mini.nvim",
+		"rcarriga/nvim-notify",
 	},
 }
 
@@ -54,6 +55,65 @@ function M.config()
 				-- lua = false, -- to disable a format, set to `false`
 			},
 		},
+		lsp = {
+			progress = {
+				enabled = true,
+				-- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+				-- See the section on formatting for more details on how to customize.
+				--- @type NoiceFormat|string
+				format = "",
+				--- @type NoiceFormat|string
+				format_done = "lsp_progress_done",
+				throttle = 1000 / 30, -- frequency to update lsp progress message
+				view = "notify",
+			},
+			override = {
+				-- override the default lsp markdown formatter with Noice
+				["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+				-- override the lsp markdown formatter with Noice
+				["vim.lsp.util.stylize_markdown"] = false,
+				-- override cmp documentation with Noice (needs the other options to work)
+				["cmp.entry.get_documentation"] = false,
+			},
+			hover = {
+				enabled = true,
+				silent = false, -- set to true to not show a message if hover is not available
+				view = nil, -- when nil, use defaults from documentation
+				---@type NoiceViewOptions
+				opts = {}, -- merged with defaults from documentation
+			},
+			signature = {
+				enabled = true,
+				auto_open = {
+					enabled = true,
+					trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+					luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+					throttle = 50, -- Debounce lsp signature help request by 50ms
+				},
+				view = nil, -- when nil, use defaults from documentation
+				---@type NoiceViewOptions
+				opts = {}, -- merged with defaults from documentation
+			},
+			message = {
+				-- Messages shown by lsp servers
+				enabled = true,
+				view = "notify",
+				opts = {},
+			},
+			-- defaults for hover and signature help
+			documentation = {
+				view = "hover",
+				---@type NoiceViewOptions
+				opts = {
+					lang = "markdown",
+					replace = true,
+					render = "plain",
+					format = { "{message}" },
+					win_options = { concealcursor = "n", conceallevel = 3 },
+				},
+			},
+		},
+		-- you can enable a preset for easier configuration
 	})
 end
 
