8ba90eab13d021ae7de9d432e3116357bd125770
S4hlo <rfl.magno02@gmail.com>
2024-05-12 18:06:22 -0700

    feature(setup): update links

--- scripts/links.sh
+++ scripts/links.sh
@@ -6,9 +6,6 @@ links_setup() {
     ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
     ln -fns ~/dotfiles/.vimrc ~/.vimrc
     ln -fns ~/dotfiles/.zshrc ~/.zshrc
-    ln -fns ~/dotfiles/qutebrowser/config.py ~/.config/qutebrowser/config.py
-    ln -fns ~/dotfiles/qutebrowser/onedark.py ~/.config/qutebrowser/onedark.py
-    ln -fns ~/dotfiles/qutebrowser/quickmarks ~/.config/qutebrowser/quickmarks
     ln -fns ~/dotfiles/gh/config.yml ~/.config/gh/config.yml
     ln -fns ~/dotfiles/gh/hosts.yml ~/.config/gh/hosts.yml
     ln -fns ~/dotfiles/nvim ~/.config/nvim
@@ -16,9 +13,9 @@ links_setup() {
     ln -fns ~/dotfiles/i3/rofi/config.rasi ~/.config/rofi/config.rasi
     ln -fns ~/dotfiles/i3/polybar/launch.sh ~/.config/polybar/launch.sh
     ln -fns ~/dotfiles/i3/polybar/config.ini ~/.config/polybar/config.ini
+    ln -fns ~/dotfiles/i3/dunstrc ~/.config/dunst/dunstrc
     ln -fns ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
     ln -fns ~/dotfiles/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
-    ln -fns ~/dotfiles/i3/dunstrc ~/.config/dunst/dunstrc
 
     tmux source-file ~/.tmux.conf
     zsh -c "source ~/.zshrc"
