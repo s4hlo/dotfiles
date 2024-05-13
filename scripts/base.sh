c93294e9b46c8f86e926b0d0b12c4f9b8aed30f0
S4hlo <rfl.magno02@gmail.com>
2024-05-12 18:15:55 -0700

    fix(crm): remove no necessary installation

--- scripts/base.sh
+++ scripts/base.sh
@@ -20,35 +20,32 @@ base_setup() {
     echo " ✅- TOOLS --> XINPUT, XPROP, XDOTOOL installation finished"
 
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | sh
-    echo " ✅ - NVM installation finished (1/8)"
+    echo " ✅ - NVM installation finished (1/7)"
 
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-    echo " ✅ - KITTY installation finished (2/8)"
+    echo " ✅ - KITTY installation finished (2/7)"
 
     curl -sS https://starship.rs/install.sh | sh
-    echo " ✅ - STARSHIP installation finished (3/8)"
+    echo " ✅ - STARSHIP installation finished (3/7)"
 
     # xclip is necessary to enable shared clipboard but only works in x11 system
     # ! CONFIG TOOL - to check where use it, search for ocurrences using grep
     sudo apt install xclip
-    echo " ✅- XCLIP installation finished (4/8)"
+    echo " ✅- XCLIP installation finished (4/7)"
 
     # this assure nvim to be installed 
     # with the latest version                           [update version]
     # TODO add a path to nvim                                  V
     wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz -P ~/Downloads
     tar xzvf ~/Downloads/nvim-linux64.tar.gz -C ~/dotfiles
-    echo " ✅ - NVIM installation finished (5/8)"
+    echo " ✅ - NVIM installation finished (5/7)"
 
     sudo apt install tmux
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    echo " ✅ - TMUX installation finished (6/8)"
+    echo " ✅ - TMUX installation finished (6/7)"
 
     # ripgrep is necessary to enable search in files using telescope
     sudo apt install ripgrep
-    echo " ✅ - RIPGREP installation finished (7/8)"
+    echo " ✅ - RIPGREP installation finished (7/7)"
 
-    # xrandr is necessary to enable screen configuration
-    sudo apt install xrandr
-    echo " ✅ - XRANDR installation finished (8/8)"
 }
