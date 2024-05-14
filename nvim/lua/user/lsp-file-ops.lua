ba051e40f96768db3d55ed3cf68a06a69596ab28
S4hlo <rfl.magno02@gmail.com>
2024-05-14 04:08:33 -0700

    feature(nvim): add lsp-file-operations plugin

---
+++ nvim/lua/user/lsp-file-ops.lua
@@ -0,0 +1,16 @@
+local M = {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
+    },
+}
+
+function M.config()
+    require("lsp-file-operations").setup()
+end
+
+return M
+
+
+
