571ffc34df01a56f9139d062acb6d12e5fde9bbe
S4hlo <rfl.magno02@gmail.com>
2024-05-16 09:32:26 -0700

    feature(setup): update version of nerd font

--- scripts/font.sh
+++ scripts/font.sh
@@ -2,7 +2,7 @@ font_setup() {
     wget -O ~/Downloads/TempFile.zip https://download-cdn.jetbrains.com/fonts/JetBrainsMono-2.304.zip
     sudo unzip -j -o ~/Downloads/TempFile.zip '*.ttf' -d /usr/share/fonts/
     rm ~/Downloads/TempFile.zip
-    wget -O ~/Downloads/TempFile.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+    wget -O ~/Downloads/TempFile.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.0/JetBrainsMono.zip
     sudo unzip -j -o ~/Downloads/TempFile.zip '*.ttf' -d /usr/share/fonts/
     rm ~/Downloads/TempFile.zip
     sudo fc-cache -fv
