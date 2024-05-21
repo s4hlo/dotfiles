b2033373e5e35643bdb54965d3feb0c8034d616e
S4hlo <rfl.magno02@gmail.com>
2024-05-21 18:40:27 +0000

    fix(nvim): remove no needed columns in noetree

--- nvim/lua/user/neo-tree.lua
+++ nvim/lua/user/neo-tree.lua
@@ -107,19 +107,19 @@ M.config = function()
           },
           -- If you don't want to use these columns, you can set `enabled = false` for each of them individually
           file_size = {
-            enabled = true,
+            enabled = false,
             required_width = 64, -- min width of window required to show this column
           },
           type = {
-            enabled = true,
+            enabled = false,
             required_width = 122, -- min width of window required to show this column
           },
           last_modified = {
-            enabled = true,
+            enabled = false,
             required_width = 88, -- min width of window required to show this column
           },
           created = {
-            enabled = true,
+            enabled = false,
             required_width = 110, -- min width of window required to show this column
           },
           symlink_target = {
