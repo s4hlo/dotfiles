0c6e39c385a1c9e383ee772025feee320a189776
S4hlo <rfl.magno02@gmail.com>
2024-07-13 15:14:22 -0400

    feat: add copilot status and remove tabs

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -1,6 +1,9 @@
 local M = {
   "nvim-lualine/lualine.nvim",
   commit = "7533b0ead663d80452210c0c089e5105089697e5",
+  dependencies = {
+    { 'AndreM222/copilot-lualine' }
+  },
 }
 
 function M.config()
@@ -89,66 +92,20 @@ function M.config()
     },
 
     sections = {
-      -- lualine_a = { {"branch", icon =""} },
-      -- lualine_b = { diff },
-      -- lualine_c = { "diagnostics" },
-      -- lualine_x = { copilot },
-      -- lualine_y = { "filetype" },
-      -- lualine_z = { "progress" },
       lualine_a = { "mode" },
-      lualine_b = {
-        {
-          "buffers",
-          show_filename_only = true, -- Shows shortened relative path when set to false.
-          hide_filename_extension = false, -- Hide filename extension when set to true.
-          show_modified_status = true, -- Shows indicator when the buffer is modified.
-
-          mode = 2, -- 0: Shows buffer name
-          -- 1: Shows buffer index
-          -- 2: Shows buffer name + buffer index
-          -- 3: Shows buffer number
-          -- 4: Shows buffer name + buffer number
-
-          -- max_length = vim.o.columns, -- Maximum width of buffers component,
-          max_length = 180, -- Maximum width of buffers component,
-          -- it can also be a function that returns
-          -- the value of `max_length` dynamically.
-          filetype_names = {
-            TelescopePrompt = "Telescope",
-            dashboard = "Dashboard",
-            packer = "Packer",
-            fzf = "FZF",
-            alpha = "Alpha",
-          }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
-
-          -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
-          use_mode_colors = false,
-
-          buffers_color = {
-            -- Same values as the general color option can be used here.
-            active = "lualine_b_normal", -- Color for active buffer.
-            inactive = "lualine_b_inactive", -- Color for inactive buffer.
-          },
-
-          symbols = {
-            modified = " ●", -- Text to show when the buffer is modified
-            alternate_file = "", -- Text to show to identify the alternate file
-            directory = "", -- Text to show when the buffer is a directory
-          },
+      lualine_b = {'filename'},
+      lualine_c = {'diff'},
+      lualine_y = {
+        { "filetype", icon_only = true },
+         {
+            'copilot',
+            show_colors = true,
         },
       },
-      lualine_c = {},
-
       lualine_x = {
-        -- copilot,
         "diagnostics",
       },
-      -- lualine_x = { "branch" },
-      lualine_y = {
-        copilot,
-        { "filetype", icon_only = true },
-      },
-      lualine_z = { "progress" },
+      lualine_z = { "progress"},
     },
     extensions = { "quickfix", "man", "fugitive" },
   }
