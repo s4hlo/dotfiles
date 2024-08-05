5c656d9f8db692b8df15f65ccefab29232806f3a
S4hlo <rfl.magno02@gmail.com>
2024-08-05 16:33:49 -0300

    feat(nvim): outline folders in neotree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -120,8 +120,8 @@ M.config = function()
 				expander_highlight = "NeoTreeExpander",
 			},
 			icon = {
-				folder_closed = "",
-				folder_open = "",
+				folder_closed = " ",
+				folder_open = " ",
 				folder_empty = "󰜌",
 				-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
 				-- then these will never be used.
