52a03e19962a2ea380ac893ec0a00b284d27c621
S4hlo <rfl.magno02@gmail.com>
2024-08-13 16:21:21 -0300

    feat(nvim): add a better color in vsplit

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,13 +5,16 @@ return {
 		priority = 1000,
 		opts = {},
 		config = function()
-			vim.cmd.colorscheme("tokyonight-night")
 			require("tokyonight").setup({
+				on_colors = function(c)
+					c.border = c.blue0
+				end,
 				style = "night",
 				-- on_colors = function(colors)
 				-- 	colors.border = "#565f89"
 				-- end,
 			})
+			vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
 }
