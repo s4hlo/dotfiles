074e80077e8d222f05be8e46f44eb72f0398ff4f
S4hlo <rfl.magno02@gmail.com>
2024-10-14 14:08:29 -0300

    push nvim config from arch branch

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -14,6 +14,9 @@ function M.config()
 	null_ls.setup({
 		debug = true,
 		sources = {
+      -- CONFIG TOOL -
+      -- add more formatters here as needed +
+      formatting.clang_format,
 			formatting.stylua,
 			formatting.prettier.with({
 				extra_filetypes = { "typescriptreact", "javascriptreact", "md", "json", "yaml", "toml" },
