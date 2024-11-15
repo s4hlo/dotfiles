88bfb6bcfbed32db9095e08ecfb10ea30c64ef7b
S4hlo <rfl.magno02@gmail.com>
2024-11-15 21:33:39 +0300

    teste

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -26,4 +26,4 @@ keymap("v", ">", ">gv", opts)
 keymap("x", "p", [["_dP]])
 
 -- THIS IS THE MOST IMPORTANT REMAP THAT I'VE EVER DONE
-keymap("i", "jk", "<Esc>", opts)
+-- keymap("i", "jk", "<Esc>", opts)
