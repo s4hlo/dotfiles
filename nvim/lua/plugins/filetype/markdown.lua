e04a468b0c465c65516945fc2c2f2986aac1148f
S4hlo <rfl.magno02@gmail.com>
2025-04-01 17:01:02 -0300

    style: remove dead code

--- nvim/lua/plugins/filetype/markdown.lua
+++ nvim/lua/plugins/filetype/markdown.lua
@@ -1,22 +1,3 @@
--- return {
--- 	{
--- 		"MeanderingProgrammer/markdown.nvim",
--- 		main = "render-markdown",
--- 		opts = {},
--- 		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
--- 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
--- 	},
--- 	{
--- 		"iamcco/markdown-preview.nvim",
--- 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
--- 		build = "cd app && yarn install",
--- 		init = function()
--- 			vim.g.mkdp_filetypes = { "markdown" }
--- 		end,
--- 		ft = { "markdown" },
--- 	},
--- }
-
 return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
