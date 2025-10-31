2a2ccf7a10ee6da5c8856089f541a39b3d5e66ab
s4hlo <rafael.nunes@tarken.ag>
2025-10-30 23:03:24 -0300

    change colorshcem

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,9 +2,16 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
+		lazy = true,
+		priority = 100,
+		opts = {},
+	},
+	{
+		"maxmx03/solarized.nvim",
+		name = "solarized",
 		lazy = false,
 		priority = 1000,
 		opts = {},
-		config = function() vim.cmd.colorscheme("catppuccin-mocha") end,
+		config = function() vim.cmd.colorscheme("solarized") end,
 	},
 }
