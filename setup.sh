b08487982de2c85ff10f434487d9fe7048ec8501
s4hlo <rafael.nunes@tarken.ag>
2025-10-30 23:36:44 -0300

    we still need some refactor here

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ log() {
 }
 
 
-install() {
+install_with_yay() {
     if [ -f /etc/arch-release ]; then
         if ! command -v yay &>/dev/null; then
             log "yay not found. Installing..." 1
@@ -46,98 +46,8 @@ install() {
         fi
         yay -S --needed $1
     else
-        # TODO: install $1 on other distros
         log "Package installation not implemented for this distribution." 2
     fi
-}
-
-configure_hyprland() {
-    log "Starting Hyprland configuration..." 0
-
-    install "hyprland xdg-desktop-portal-hyprland"
-    install "hyprpaper waybar hyprshade hyprshot wofi"
-    
-    # check if those two are installed with the above when zero setup
-    # hyprlock
-    # hyprpicker-git \
-
-    rm -rf -- ~/.config/hypr && ln -sfn ~/dotfiles/hypr ~/.config/hypr
-    rm -rf -- ~/.config/waybar && ln -sfn ~/dotfiles/waybar ~/.config/waybar
-    rm -rf -- ~/.config/wofi && ln -sfn ~/dotfiles/wofi ~/.config/wofi
-
-    log "Hyprland configuration completed!" 0
-}
-
-configure_i3() {
-    log "Starting i3 configuration..." 0
-
-    install "i3 picom polybar rofi xwallpaper xorg-xinput"
-
-    mkdir ~/.config/i3
-
-    rm -rf -- ~/.config/i3/config && ln -sfn ~/dotfiles/i3/config ~/.config/i3/config
-    rm -rf -- ~/.config/rofi && ln -sfn ~/dotfiles/i3/rofi ~/.config/rofi
-    rm -rf -- ~/.config/polybar && ln -sfn ~/dotfiles/i3/polybar ~/.config/polybar
-
-    log "i3 configuration completed!" 0
-}
-
-add_wsl() {
-    log "Installing WSL packages..." 0
-    install $(cat ~/dotfiles/minimal.list)
-    log "WSL packages installed!" 0
-}
-
-link_wsl() {
-    log "Creating WSL symlinks..." 0
-
-    rm -rf -- ~/.bashrc && ln -sfn ~/dotfiles/.bashrc ~/.bashrc
-    rm -rf -- ~/.gitconfig && ln -sfn ~/dotfiles/.gitconfig ~/.gitconfig
-    rm -rf -- ~/.tmux.conf && ln -sfn ~/dotfiles/.tmux.conf ~/.tmux.conf
-    rm -rf -- ~/.zshrc && ln -sfn ~/dotfiles/.zshrc ~/.zshrc
-    rm -rf -- ~/.config/bat && ln -sfn ~/dotfiles/bat ~/.config/bat
-    rm -rf -- ~/.config/btop && ln -sfn ~/dotfiles/btop ~/.config/btop
-    rm -rf -- ~/.config/gh && ln -sfn ~/dotfiles/gh ~/.config/gh
-
-    # identify shell and source it
-    if [ "$SHELL" = "/bin/zsh" ]; then
-        source ~/.zshrc
-    else
-        source ~/.bashrc
-    fi
-    
-    log "WSL symlinks created!" 0
-}
-
-add_full() {
-    log "Installing all packages..." 0
-    install $(cat ~/dotfiles/pkg.list)
-    install $(cat ~/dotfiles/pkg_aur.list)
-    log "All packages installed!" 0
-}
-
-
-links_setup() {
-    log "Creating full symlinks setup..." 0
-    
-    link_wsl
-
-    rm -rf -- ~/.config/nvim && ln -sfn ~/dotfiles/nvim ~/.config/nvim
-    rm -rf -- ~/.config/kitty && ln -sfn ~/dotfiles/kitty ~/.config/kitty
-    rm -rf -- ~/.config/Cursor/User/settings.json && ln -sfn ~/dotfiles/cursor/settings.json ~/.config/Cursor/User/settings.json
-    rm -rf -- ~/.config/Cursor/User/keybindings.json && ln -sfn ~/dotfiles/cursor/keybindings.json ~/.config/Cursor/User/keybindings.json
-    rm -rf -- ~/.config/swaync && ln -sfn ~/dotfiles/swaync ~/.config/swaync
-
-    if [ "$SHELL" != "/bin/zsh" ]; then
-        sudo chsh -s $(which zsh)
-        chsh -s $(which zsh)
-    fi
-
-    sudo timedatectl set-timezone Etc/GMT-3
-    tmux source-file ~/.tmux.conf
-    zsh -c "source ~/.zshrc"
-    
-    log "Full symlinks setup completed!" 0
 }
 
 show_menu() {
@@ -152,37 +62,6 @@ show_menu() {
     echo -e "\e[95m  7)\e[0m \e[91mexit\e[0m       Exit the setup                           \e[0m"
     echo -e ""
     echo -e "\e[34m> Enter your choice (1-7): \e[0m"
-}
-
-run_command() {
-    case "$1" in
-    1 | wsl)
-        add_wsl
-        ;;
-    2 | full)
-        add_full
-        ;;
-    3 | hypr)
-        configure_hyprland
-        ;;
-    4 | i3)
-        configure_i3
-        ;;
-    5 | link)
-        links_setup
-        ;;
-    6 | link_wsl)
-        link_wsl
-        ;;
-    7 | exit)
-        echo -e "\e[32mExiting setup. Goodbye!\e[0m"
-        exit 0
-        ;;
-    *)
-        log "Invalid option. Please choose 1-7." 2
-        return 1
-        ;;
-    esac
 }
 
 ask_continue() {
@@ -204,49 +83,119 @@ while true; do
     show_menu
     read -r choice
 
-    if run_command "$choice"; then
+    case "$choice" in
+    1 | wsl)
+        log "Installing WSL packages..." 0
+        install_with_yay $(cat ~/dotfiles/minimal.list)
+        log "WSL packages installed!" 0
         if ! ask_continue; then
             echo -e "\e[32mSetup completed. Goodbye!\e[0m"
-
             break
         fi
-    fi
-done
+        log "Creating WSL symlinks..." 0
 
-echo -e '
-#################################################################################
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠓⢤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-   SETUP  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀
-   COMPLETE!   ⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡀⡀⠀⠀⠀⢹⠀⠀⠀⠈⢷⡀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿⠀⠀⢠⠄⠀⠀⠀⠀⠀⢤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⢻⡄⠀⠀⠘⡆⠀⠀⠀⠘⣧⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⠃⠀⢀⡇⠀⠀⠀⠀⠀⠀⡟⡆⠀⠀⠀⠀⠀⠀⢀⣴⠖⠀⠀⢠⠇⠀⠀⠀⠀⠀⢀⡸⣿⡇⠀⠀⠀⡇⠀⡄⠀⠀⠘⣆⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠁⠀⠀⢸⡇⠀⣴⠀⠀⠀⠀⣿⡏⢸⠃⠀⠀⠀⢀⣾⠁⠀⠀⠀⣾⣇⠀⠀⠀⠀⠀⠘⡇⣿⢹⠀⠀⠀⠇⠀⡇⠀⠀⠀⣿⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣏⠀⠀⠀⠀⣿⠃⢰⠃⠀⠀⠀⢸⡟⡅⡏⣀⠀⠀⠀⣼⠇⠀⠀⠀⢀⣿⠸⡴⡄⠀⠀⠀⠀⢹⢹⣟⠀⠀⠀⠀⠀⡇⠀⠀⢠⠈⡇⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠟⣽⠃⠀⠀⠀⣸⣿⠀⡎⠀⠀⠀⠀⣿⢳⣷⢱⣿⢀⣸⣷⢻⣴⠂⠀⠀⣸⠃⠀⢻⡹⣠⡀⠀⢦⠈⣞⣿⠀⠀⠀⠀⠀⡇⠀⠀⢸⡄⡇⠀⠀⠀⠀
-⢤⣀⠀⠀⠀⠀⢀⣠⣶⢿⠑⡀⣿⠀⠀⠀⢀⡿⡇⢸⣇⡶⠀⠀⣶⣟⣸⣏⣿⡿⣾⣿⠏⣸⡿⡄⠀⢠⣿⣤⣤⣶⣧⢷⣧⣆⠘⣇⢹⣽⡀⠀⠀⠀⠀⡇⡀⠀⠈⣿⣷⠀⠀⠀⠀
-⠀⠈⠉⠙⢦⡀⢸⡏⣿⡼⢸⢣⣿⠀⠀⠀⢸⣸⡇⣾⣾⠃⣰⣶⣿⠿⣿⡿⣿⢷⣿⡏⠀⣧⢷⣇⡀⢸⠏⠉⠀⠀⠈⢻⣿⣿⣦⢹⣦⢻⣧⠀⠀⣿⢠⡇⠹⡄⠀⣿⡿⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠻⣼⡇⣿⡇⣸⣸⣿⠀⠀⠀⣿⣿⣿⣿⡏⣰⣿⣿⠇⠀⡟⢡⣿⡾⡿⠀⠀⣿⣾⣿⣷⡟⠀⠀⠀⠀⠀⢀⣻⣿⣿⣿⣿⣿⣿⡇⠀⡟⢹⣷⣄⢳⡀⣿⣧⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢻⣷⣿⣿⣿⡄⠀⢐⣿⣿⣿⣽⣿⣿⣿⣯⣤⣾⡀⠺⠿⠟⠁⠀⠀⢻⣿⣿⣿⡇⠀⣠⣴⣿⣯⣿⣶⣿⣿⣿⣉⣀⣿⡇⠀⠇⢸⡏⠁⠻⣷⢹⡇⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⢷⡼⣿⡇⠀⠘⣿⣿⠃⣿⣿⣽⣿⣿⣾⣿⣽⣶⠀⠀⠀⠀⠀⠀⠉⠉⠉⠀⠀⣿⡿⣯⣯⣿⣿⣿⡍⠙⠛⠛⣽⡇⠀⡄⢾⡃⢠⡄⠹⣿⡇⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡿⢡⣿⡇⠀⠀⡿⣿⠿⠛⠉⢡⣿⣿⣿⣿⣎⠉⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠋⠀⠻⡽⣿⣿⣿⡇⠀⠀⡒⣹⠁⠀⠀⣸⠹⠧⠚⠈⣿⠁⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⢼⣿⡇⠀⠀⣿⣇⡀⠀⠀⠸⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠋⠛⠿⠯⠀⠀⠀⠀⢸⠀⠀⠀⣿⣤⠒⠛⠁⣿⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⢿⡇⠀⠀⣿⠀⠐⠲⡤⠤⡬⡿⠿⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⡀⣺⠘⣆⢠⢰⡟⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⠘⢾⡇⢀⣠⣿⢤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⡃⣿⠴⠛⢈⡾⠃⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⡼⠛⠋⢉⣤⠖⠻⢦⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⡇⡿⠀⠀⡾⠃⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⣀⡴⠖⢹⡶⠚⠉⡽⠟⠓⠚⠉⢹⣆⠀⠀⠀⠀⠙⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⣧⣧⣤⣾⠇⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⡾⠁⠀⠀⣸⠃⠀⠀⡇⠀⠀⢲⠀⠁⠙⣆⠀⠀⠀⠀⠈⠻⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢿⣿⣾⡏⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⣿⠁⠀⠀⢷⠀⠀⠀⢹⠀⠀⢰⠃⠀⠀⢸⣧⠀⠀⠘⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡇⠀⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠻⣆⠀⠀⢸⡀⠀⠀⢸⠀⠀⢸⠀⠀⠀⠸⡮⠷⣄⣀⡼⢢⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⡇⠀⣾⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⡀⠀⣧⠀⠀⢃⠀⠀⣞⠀⠀⠀⠰⠟⠀⠈⠙⠲⣤⣭⣍⡉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣽⠃⠀⣿⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⢹⠀⠀⡏⠀⢨⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠉⠙⠲⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠁⢸⣾⠀⠀⣿⡄⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡇⠀⠀⠀⠀⠙⠲⠶⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⢀⣠⠾⠋⠀⠀⢸⣿⠀⢰⣿⠃⠈⣷⢤⣀⠀⠀⠀⠀⠀⠀
-⠀⣀⣠⡤⠤⢶⣦⣤⣤⡤⣄⣀⡀⢿⣄⠙⠛⠓⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀⣀⡴⠛⠁⠀⠀⠀⠀⠀⣿⠃⢸⠇⠀⠀⢸⠀⠈⢷⡤⣄⡀⠀⠀
-⠉⠙⠢⠤⠔⠉⠁⠈⠉⠉⠒⠺⠯⠽⣯⠙⣷⣶⢦⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡶⠞⠉⠀⠀⠀⠀⠀⠀⠀⠀⡿⠀⣾⠀⠀⠀⢸⠀⠀⠀⠹⣄⠙⠓⢤
-⠀⠀⠀⠀⠀⠀⠀⠶⣄⠀⠀⠀⠀⠀⣿⡇⢹⣿⠈⣯⡏⠉⠓⠢⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⠀⡟⠀⠀⠀⢸⡖⠒⠦⣄⠈⠙⢦⡀
-⠀⠀⠀⠀⠀⠀⠀⠀⠈⢣⡀⠀⠀⠀⢸⣵⠀⣿⡄⠘⢿⠀⠀⠀⠀⠉⠑⢶⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⢀⣰⠟⢹⣼⢰⠇⠀⠀⠀⢸⣷⡀⠀⠈⠓⢦⡀⠈
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⣄⠀⠱⡀⣿⡇⣻⣹⡀⢸⡀⠀⠀⠀⠀⠀⠀⡇⠈⠉⠛⠿⠿⠤⠴⠂⠀⣾⠀⠀⠀⠀⠀⣠⠔⠋⠁⠀⣸⡿⣸⠀⠀⠀⠀⢸⢿⡇⠀⠀⠀⠀⠉⢀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀⢻⣹⣿⣹⣧⡇⠀⢧⡀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣏⠀⣀⡤⠖⠋⡀⠀⠀⠀⣀⣿⣿⡏⠀⠀⠀⢀⡞⠘⡇⠀⠀⠠⣀⠀⠀
-#################################################################################
+        rm -rf -- ~/.bashrc && ln -sfn ~/dotfiles/.bashrc ~/.bashrc
+        rm -rf -- ~/.gitconfig && ln -sfn ~/dotfiles/.gitconfig ~/.gitconfig
+        rm -rf -- ~/.tmux.conf && ln -sfn ~/dotfiles/.tmux.conf ~/.tmux.conf
+        rm -rf -- ~/.zshrc && ln -sfn ~/dotfiles/.zshrc ~/.zshrc
+        rm -rf -- ~/.config/bat && ln -sfn ~/dotfiles/bat ~/.config/bat
+        rm -rf -- ~/.config/btop && ln -sfn ~/dotfiles/btop ~/.config/btop
+        rm -rf -- ~/.config/gh && ln -sfn ~/dotfiles/gh ~/.config/gh
+
+        source ~/.bashrc
+        
+        log "WSL symlinks created!" 0
+        if ! ask_continue; then
+            echo -e "\e[32mSetup completed. Goodbye!\e[0m"
+            break
+        fi
+        ;;
+
+    2 | full)
+        log "Installing all packages..." 0
+        install_with_yay $(cat ~/dotfiles/pkg.list)
+        install_with_yay $(cat ~/dotfiles/pkg_aur.list)
+        log "All packages installed!" 0
+        if ! ask_continue; then
+            echo -e "\e[32mSetup completed. Goodbye!\e[0m"
+            break
+        fi
+        ;;
+    3 | hypr)
+        log "Starting Hyprland configuration..." 0
+        install_with_yay "hyprland xdg-desktop-portal-hyprland"
+        install_with_yay "hyprpaper waybar hyprshade hyprshot wofi"
+        
+        # check if those two are installed with the above when zero setup
+        # hyprlock
+        # hyprpicker-git \
+
+        rm -rf -- ~/.config/hypr && ln -sfn ~/dotfiles/hypr ~/.config/hypr
+        rm -rf -- ~/.config/waybar && ln -sfn ~/dotfiles/waybar ~/.config/waybar
+        rm -rf -- ~/.config/wofi && ln -sfn ~/dotfiles/wofi ~/.config/wofi
+
+        log "Hyprland configuration completed!" 0
+        if ! ask_continue; then
+            echo -e "\e[32mSetup completed. Goodbye!\e[0m"
+            break
+        fi
+        ;;
+    4 | i3)
+        log "Starting i3 configuration..." 0
+        install_with_yay "i3 picom polybar rofi xwallpaper xorg-xinput"
+
+        mkdir ~/.config/i3
+
+        rm -rf -- ~/.config/i3/config && ln -sfn ~/dotfiles/i3/config ~/.config/i3/config
+        rm -rf -- ~/.config/rofi && ln -sfn ~/dotfiles/i3/rofi ~/.config/rofi
+        rm -rf -- ~/.config/polybar && ln -sfn ~/dotfiles/i3/polybar ~/.config/polybar
+
+        log "i3 configuration completed!" 0
+        if ! ask_continue; then
+            echo -e "\e[32mSetup completed. Goodbye!\e[0m"
+            break
+        fi
+        ;;
+    5 | link)
+        log "Creating full symlinks setup..." 0
+        
+        log "Creating WSL symlinks..." 0
+        rm -rf -- ~/.bashrc && ln -sfn ~/dotfiles/.bashrc ~/.bashrc
+        rm -rf -- ~/.gitconfig && ln -sfn ~/dotfiles/.gitconfig ~/.gitconfig
+        rm -rf -- ~/.tmux.conf && ln -sfn ~/dotfiles/.tmux.conf ~/.tmux.conf
+        rm -rf -- ~/.zshrc && ln -sfn ~/dotfiles/.zshrc ~/.zshrc
+        rm -rf -- ~/.config/bat && ln -sfn ~/dotfiles/bat ~/.config/bat
+        rm -rf -- ~/.config/btop && ln -sfn ~/dotfiles/btop ~/.config/btop
+        rm -rf -- ~/.config/gh && ln -sfn ~/dotfiles/gh ~/.config/gh
+
+        rm -rf -- ~/.config/nvim && ln -sfn ~/dotfiles/nvim ~/.config/nvim
+        rm -rf -- ~/.config/kitty && ln -sfn ~/dotfiles/kitty ~/.config/kitty
+        rm -rf -- ~/.config/Cursor/User/settings.json && ln -sfn ~/dotfiles/cursor/settings.json ~/.config/Cursor/User/settings.json
+        rm -rf -- ~/.config/Cursor/User/keybindings.json && ln -sfn ~/dotfiles/cursor/keybindings.json ~/.config/Cursor/User/keybindings.json
+        rm -rf -- ~/.config/swaync && ln -sfn ~/dotfiles/swaync ~/.config/swaync
+
+        if [ "$SHELL" != "/bin/zsh" ]; then
+            sudo chsh -s $(which zsh)
+            chsh -s $(which zsh)
+        fi
+
+        sudo timedatectl set-timezone Etc/GMT-3
+        tmux source-file ~/.tmux.conf
+        zsh -c "source ~/.zshrc"
+        
+        log "Full symlinks setup completed!" 0
+        if ! ask_continue; then
+            echo -e "\e[32mSetup completed. Goodbye!\e[0m"
+            break
+        fi
+        ;;
+    6 | exit)
+        echo -e "\e[32mExiting setup. Goodbye!\e[0m"
+        exit 0
+        ;;
+    *)
+        log "Invalid option. Please choose 1-7." 2
+        ;;
+    esac
+done
 '
