ed2ba7587a0bda92e12607b8b226abe8393a90cf
s4hlo <rafael.nunes@tarken.ag>
2025-11-12 23:09:10 -0300

    smalls in setup wsl

--- swsl.sh
+++ swsl.sh
@@ -7,7 +7,6 @@ log() {
     local type=${2:-0}
     local colors=("\e[32m" "\e[33m" "\e[31m" "\e[31m")
     local labels=("[INFO]" "[WARN]" "[ERROR]" "[EXIT]")
-
     echo -e "${colors[$type]}${labels[$type]}\e[0m - $content"
 }
 
@@ -16,20 +15,17 @@ sudo apt update && sudo apt install -y $(cat ~/dotfiles/wsl.list)
 sudo snap install nvim --classic
 sudo snap install yazi --classic
 
-if command -v starship >/dev/null 2>&1; then
-    echo "Starship $(starship --version)"
-else
-    curl -sS https://starship.rs/install.sh | sh
-fi
-
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 
 if [ -s "$NVM_DIR/nvm.sh" ]; then
     . "$NVM_DIR/nvm.sh"
 fi
 
-
-
+if command -v starship >/dev/null 2>&1; then
+    echo "Starship $(starship --version)"
+else
+    curl -sS https://starship.rs/install.sh | sh
+fi
 
 if command -v nvm >/dev/null 2>&1; then
     echo "nvm $(nvm --version)"
@@ -50,30 +46,24 @@ else
 fi
 
 log "WSL packages installed!" 0
-
 log "Creating WSL symlinks..." 0
-
 mkdir -p ~/.config
-
 rm -rf -- ~/.bashrc && ln -sfn ~/dotfiles/.bashrc ~/.bashrc
 rm -rf -- ~/.gitconfig && ln -sfn ~/dotfiles/.gitconfig ~/.gitconfig
 rm -rf -- ~/.tmux.conf && ln -sfn ~/dotfiles/.tmux.conf ~/.tmux.conf
 rm -rf -- ~/.zshrc && ln -sfn ~/dotfiles/.zshrc ~/.zshrc
-
 rm -rf -- ~/.config/bat && ln -sfn ~/dotfiles/bat ~/.config/bat
 rm -rf -- ~/.config/btop && ln -sfn ~/dotfiles/btop ~/.config/btop
 rm -rf -- ~/.config/gh && ln -sfn ~/dotfiles/gh ~/.config/gh
 rm -rf -- ~/.config/vim && ln -sfn ~/dotfiles/nvim ~/.config/nvim
-
 log "WSL symlinks created successfully!" 0
 
-
+log "Start execution setup" 0
 source ~/.bashrc
 nvm install --lts
 nvm use --lts
 sudo timedatectl set-timezone Etc/GMT-3
 tmux source-file ~/.tmux.conf
-
 log "WSL setup completed!" 0
 
 log "RUN source ~/.bashrc IF ANYTHING DONT WORK"
